0af478fd2ba9e1037d16450638097eff76a5ab3a
devnishant <ncp1435@gmail.com>
2025-07-27 19:52:27 +0530

    K8 for Developers

---
+++ Kubernetes/07_Config_Map/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000
+
+const appName = process.env.APP_NAME || test
+
+app.get('/',(req,res)=>
+{
+    res.send(`Hello World from dockerized app ${appName}`);
+})
+
+app.listen(port,()=>
+{
+    console.log('listening to the port', port);
+})
