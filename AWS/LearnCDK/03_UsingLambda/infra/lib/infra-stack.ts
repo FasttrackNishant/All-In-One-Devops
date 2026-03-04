ee287bf28eb198def56e76445706c0e7d96056ba
Nishant Patil <ncp1435@gmail.com>
2026-03-04 23:05:48 +0530

    Cloud Watch Alarm

--- AWS/LearnCDK/03_UsingLambda/infra/lib/infra-stack.ts
+++ AWS/LearnCDK/03_UsingLambda/infra/lib/infra-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch'
+import { Alias } from 'aws-cdk-lib/aws-kms';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -14,5 +16,11 @@ export class InfraStack extends cdk.Stack {
       code : lambda.Code.fromAsset('../services/'),
       functionName : 'testcdklambdafirst'
     });
+
+    const cloudWatchInstance = new cloudwatch.Alarm(this,'cloudwatchlogicalid',{
+      evaluationPeriods:1,
+      threshold:1,
+      metric: lambdaInstance.metricErrors()
+    })
   }
 }
